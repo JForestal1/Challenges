b42ebc7e0a0d175204239069a2a6c72c93aa2025
JForestal1 <jforestal1@gmail.com>
2020-11-17 19:44:55 -0500

    innitial commit

---
+++ IfElseNesting/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElseNesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hoursOfSleep = 5;
+
+            if (hoursOfSleep > 9)
+            {
+                Console.WriteLine("Wow, thats a lot of sleep");
+            }
+            else if(hoursOfSleep == 9)
+            {
+                Console.WriteLine("You are well rested");
+            }
+            else if (hoursOfSleep / 4 == 1)
+            {
+                Console.WriteLine("Bummer");
+            }
+            else
+            {
+                Console.WriteLine("Get Some Sleep");
+            }
+
+            string superLongString = "thisisaveryverylongstringwithmanyvowels";
+            foreach (char letter in superLongString)
+                if (letter == 'a')
+                    Console.WriteLine(letter);
+        }
+    }
+}
